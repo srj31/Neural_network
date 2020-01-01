56b998c3b933b944fcd235c21509927073e0cfa3
Sourabh Jaiswal <srj31@Sourabhs-MacBook-Air.local>
2020-01-01 15:17:55 +0530

    adding files

---
+++ Preprocessing/logistic_regression.py
@@ -0,0 +1,36 @@
+
+
+def sigmoid(z):
+	"""
+		To calculate the sigmoid of z
+	"""
+
+	s = 1/(1+np.exp(-z))
+
+	return s
+
+
+def propagation(w,b,X,Y):
+	"""
+		Implementing the cost function and its gradient for the neural network
+
+		Arguments:
+		w - weights for each neuron (num_px*num_py*3,1)
+		b - bias
+		X - input matrix
+		Y - output matrix
+
+		Return:
+		cost: the cost value for the input
+		dw: gradient with respect to w , matrix of size w
+		db: gradient with respect to b
+
+	"""
+
+	#activation function
+	A = sigmoid(np.dot(w.T,X) + b)
+	cost = -1/m*np.sum(Y*np.log(A) + (1-Y)*np.log(1-A))
+
+	dw = 1/m*np.dot(X,(A-Y).T)
+	db = 1/m*np.sum(A-Y)
+	 
